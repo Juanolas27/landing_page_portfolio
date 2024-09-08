bbe860ff39d070aec582f0904b0d387243a86655
Juanolas27 <davidbarriuso01@gmail.com>
2024-09-08 11:46:30 +0200

    Color selection and structure

---
+++ src/app/componente-dos/componente-dos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-dos',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente-dos.component.html',
+  styleUrl: './componente-dos.component.sass'
+})
+export class ComponenteDosComponent {
+
+}
